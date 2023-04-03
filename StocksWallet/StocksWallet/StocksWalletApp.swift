3cc886c53da2d0f95e75f6d95375ce7088fa764b
Felipe Menezes <fmmobile@live.com>
2023-04-03 14:48:47 -0300

    initial setup

---
+++ StocksWallet/StocksWallet/StocksWalletApp.swift
@@ -0,0 +1,20 @@
+//
+//  StocksWalletApp.swift
+//  StocksWallet
+//
+//  Created by Felipe Menezes on 29/03/23.
+//
+
+import SwiftUI
+
+@main
+struct StocksWalletApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
